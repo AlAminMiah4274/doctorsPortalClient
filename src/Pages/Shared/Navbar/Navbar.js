fd32f17421655b6763c7315e67c22c329aba4b7d
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-11-26 21:52:26 +0600

    in index.html changed page name and width size added in app.js and added some comments & modifed some info in navbar and loaded babel package in package.json & package-lock.json

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -2,42 +2,43 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+    // item names for navbar
+    const navmenuItems = <>
+        <li><Link>Home</Link></li>
+        <li><Link>Appointment</Link></li>
+        <li><Link>About</Link></li>
+        <li><Link>Login</Link></li>
+    </>
+
     return (
         <div className="navbar bg-base-100">
 
             <div className="navbar-start">
+
+                {/* dropdown menu for small devices */}
                 <div className="dropdown">
+
+                    {/* button for showing navbar menu items in small devices */}
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
+
+                    {/* navbar items for small devices */}
                     <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link>Item 1</Link></li>
-                        <li>
-                            <Link>Parent</Link>
-                            <ul className="p-2">
-                                <li><Link>Submenu 1</Link></li>
-                                <li><Link>Submenu 2</Link></li>
-                            </ul>
-                        </li>
-                        <li><Link>Item 3</Link></li>
+                        {navmenuItems}
                     </ul>
                 </div>
-                <Link to="/" className="text-2xl font-bold">Doctor Portal</Link>
-            </div>
 
+                {/* application logo or name */}
+                <Link to="/" className="text-2xl font-bold">Doctors Portal</Link>
+
+            </div>
+            
+            {/* navbar items for large devices */}
             <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link>Item 1</Link></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><Link>Submenu 1</Link></li>
-                                <li><Link>Submenu 2</Link></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><Link>Item 3</Link></li>
+                    {navmenuItems}
                 </ul>
             </div>
 
