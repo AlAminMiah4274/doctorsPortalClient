f20cb58d744b064777f965d5053ebc3082ac1dd2
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-11-28 18:14:02 +0600

    created and completed the footer component

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,42 @@
 import React from "react";
+import footerImage from "../../../assets/images/footer.png";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is Footer</h1>
-        </div>
+        <footer style={{
+            background: `url(${footerImage})`,
+            backgroundSize: 'cover'
+        }} className="p-10 text-black mt-16">
+
+            <div className="footer flex justify-around">
+                <nav>
+                    <header className="footer-title">Services</header>
+                    <Link to="/" className="link link-hover">Branding</Link>
+                    <Link to="/" className="link link-hover">Design</Link>
+                    <Link to="/" className="link link-hover">Marketing</Link>
+                    <Link to="/" className="link link-hover">Advertisement</Link>
+                </nav>
+                <nav>
+                    <header className="footer-title">Company</header>
+                    <Link to="/" className="link link-hover">About us</Link>
+                    <Link to="/" className="link link-hover">Contact</Link>
+                    <Link to="/" className="link link-hover">Jobs</Link>
+                    <Link to="/" className="link link-hover">Press kit</Link>
+                </nav>
+                <nav>
+                    <header className="footer-title">Legal</header>
+                    <Link to="/" className="link link-hover">Terms of use</Link>
+                    <Link to="/" className="link link-hover">Privacy policy</Link>
+                    <Link to="/" className="link link-hover">Cookie policy</Link>
+                </nav>
+            </div>
+
+            <div className="text-center mt-32">
+                <p>Copyright © 2023 - All right reserved by Doctors Portal</p>
+            </div>
+
+        </footer>
     );
 };
 
