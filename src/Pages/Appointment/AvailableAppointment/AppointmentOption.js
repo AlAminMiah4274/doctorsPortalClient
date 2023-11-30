3365b565c307e5f6acde3313c29818eb35f8c589
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-11-30 20:37:20 +0600

    modified some info in the appointmentOption component and created bookingModal component

--- src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
@@ -11,12 +11,19 @@ const AppointmentOption = ({ option }) => {
             <div className="card-body">
 
                 <h2 className="card-title justify-center">{name}</h2>
+
                 <p className="text-center">
-                    <select className="select select-bordered">
-                        {slots.map(slot => <option value={slot}>{slot}</option>)}
-                    </select>
+                    {
+                        slots.length > 0 ?
+                        <select className="select select-bordered">
+                            {slots.map(slot => <option value={slot}>{slot}</option>)}
+                        </select>
+                        :
+                        'Try Another Day'
+                    }
                 </p>
-                <p className="text-center">{slots.length} Spaces Available</p>
+                
+                <p className="text-center">{slots.length} {slots.length > 1 ? 'Spaces' : 'Space'} Available</p>
 
                 <div className="card-actions justify-center mt-10">
                     <PrimaryButton>Book Appoinment</PrimaryButton>
